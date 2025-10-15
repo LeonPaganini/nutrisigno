2109b02b4db95b4ef8cd7048b0c5158d149fddfb
LeonPaganini <121823627+LeonPaganini@users.noreply.github.com>
2025-10-15 05:40:32 -0300

    Criar __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""NutriSigno package initializer.
+
+This package contains the Streamlit application and supporting modules
+for the NutriSigno project.  It exposes no public API but simply
+marks this directory as a Python package so relative imports work.
+"""
+
+__all__: list[str] = []
