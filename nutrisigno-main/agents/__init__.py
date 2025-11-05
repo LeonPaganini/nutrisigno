cb78beffb27e05661ac60dbd81d5a2e40c7415b6
LeonPaganini <121823627+LeonPaganini@users.noreply.github.com>
2025-11-05 17:31:21 -0300

    Delete nutrisigno-main directory

--- nutrisigno-main/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Coleção de utilitários para carregar dados auxiliares do projeto."""
-
-from .diet_loader import load_diets
-from .subs_loader import load_substitutions
-
-__all__ = ["load_diets", "load_substitutions"]
